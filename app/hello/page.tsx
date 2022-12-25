3e32b5c5e0112051e1e4a408e41670312e686ce8
Genki <genk1@users.noreply.github.com>
2022-12-25 21:40:05 +0900

    day12: add not-found page
    - https://beta.nextjs.org/docs/api-reference/notfound#notfound
    - https://beta.nextjs.org/docs/api-reference/file-conventions/not-found

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,9 +1,20 @@
-import { resolve } from "path";
+import { notFound } from "next/navigation";
+
+async function fetchUser(id?: string) {
+  // const res = await fetch("https://httpstat.us/404");
+  // const res = await fetch("https://httpstat.us/500");
+  const res = await fetch("http://localhost:3000/api/hello");
+  if (!res.ok) {
+    return undefined;
+  }
+
+  return await res.json();
+}
 
 export default async function HelloPage() {
-  const req = await fetch("http://localhost:3000/api/hello");
-  // const req = await fetch("https://httpstat.us/500");
-  const data = await req.json();
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return <p>{data.name}</p>;
+  let user = await fetchUser();
+  if (!user) {
+    notFound();
+  }
+  return <p>{user.name}</p>;
 }
