a3f62ce631a44a2e0c2f63ddb68214eca1cef4ed
Genki <genk1@users.noreply.github.com>
2022-12-25 21:30:12 +0900

    day11: customize error page
    - https://beta.nextjs.org/docs/routing/error-handling#handling-server-errors

--- app/hello/error.tsx
+++ app/hello/error.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-export default function HelloError({ error }: { error: Error }) {
+import { useEffect } from "react";
+
+export default function HelloError({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
   return (
     <>
       <p>something went wrong</p>
-      {/* <p>{JSON.stringify(error, null, 2)}</p> */}
+      <p>{JSON.stringify(error, null, 2)}</p>
+      <button onClick={() => reset()}>Reset Error Boundary</button>
     </>
   );
 }
